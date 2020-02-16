bc598dc9880abf5879a92530b9629ae3f67d6453
vastlaan <vastlaan@mail.com>
2020-02-16 08:59:05 +0100

    functionality over opening new path /new with prodId selection. Item component finished, newCollection finished.

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -7,13 +7,21 @@ function Item(props) {
 	const {item} = props
 
 	const [selectedColor, setSelectedColor] = useState(item.colors[0])
+	const [selectedSize, setSelectedSize] = useState(item.sizes[0])
+	const [selectedTab, setSelectedTab] = useState('description') // possible: 'description', 'shipping', 'reviews'
 
 	const renderShoppingCard =()=>{
-
+		console.log(item.id, selectedColor, selectedSize)
+	}
+	const claculateScore = (item) =>{
+		const l = item.score.length
+		const s = item.score.reduce((acc, s)=>acc+s,0)
+		return s/l
 	}
 
   return (
     <div className="item">
+
       	<div className='item__main'>
       		<div  className='item__main--name'>
       			<h3>{item.name}</h3>
@@ -27,7 +35,7 @@ function Item(props) {
       			{
       				item.sizes.map((size,i)=>{
       					return(
-      						<div key={`${size}-${i}`}>
+      						<div key={`${size}-${i}`} onClick={()=>setSelectedSize(size)}>
       							{size}
       						</div>
       						)
@@ -52,6 +60,44 @@ function Item(props) {
       				<li><FaCheck/>Gratis verzenden & retourneren</li>
       				<li><FaCheck/>Op werkdagen voor 18.00u besteld = morgen in huis</li>
       			</ul>
+      		</div>
+      		<div className='item__main--details'>
+      			<div className='item__main--details-tabs'>
+      				<div onClick={()=>setSelectedTab('description')} className={selectedTab==='description'?'highlightedTab':''}>
+      					Omschrijving
+      				</div>
+      				<div onClick={()=>setSelectedTab('shipping')} className={selectedTab==='shipping'?'highlightedTab':''}>
+      					Verzending
+      				</div>
+      				<div onClick={()=>setSelectedTab('reviews')} className={selectedTab==='reviews'?'highlightedTab':''}>
+      					Reviews
+      				</div>
+      			</div>
+      			<div className='item__main--details-content'>
+      				{
+      					selectedTab==='description'? 
+      						<div>
+      							<h3>{item.name} - id: {item.id}</h3>
+      							<p>{item.description}</p>
+      						</div> 
+      					: selectedTab==='shipping'?
+      						<div>
+      							<h3>Verzending</h3>
+      							<p>Op werkdagen voor 18:00 uur besteld is de volgende dag in huis. Je bestelling wordt gratis bezorgd door DHL. Levering op afspraak is helaas niet mogelijk.</p>
+      							<h3>RUILEN EN RETOURNEREN</h3>
+      							<p>Wil je een artikel ruilen? Stuur het artikel/de artikelen retour en plaats vervolgens zelf een nieuwe bestelling. Wil je een artikel retourneren? Dat is mogelijk binnen 14 dagen na ontvangst van je pakket.</p>    														
+							</div> 
+      						: <div>
+      							<h3>Reviews</h3>
+      							<h4>Dit product scored:</h4>
+      							{
+      								item.score.length===0?<p>Er zijn nog geen reviews</p>:<h1>{claculateScore(item)}</h1>
+      							}
+      						</div>
+      					
+      				}
+      			</div>
+
       		</div>
       	</div>
 	     
--- client/src/components/NewCollection.js
+++ client/src/components/NewCollection.js
@@ -17,19 +17,39 @@ function NewCollection(props) {
 	console.log(products, item)
 
 	return(
-		 <div className="newCollection" style={{marginTop: '17rem'}}>
+		 <div className="newCollection" style={{marginTop: '17rem', paddingTop:'5rem'}}>
 		      
-	      	<h1 className="newCollection__header">Nieuw Collectie</h1>
+	      	
 	      	{item?<Item item={item} lang={props.lang}/>:null}
-	      	<div className='newCollection__all'>
+
+	      	<h1 className="newCollection__header">Nieuw Collectie</h1>
+
+	      	<div className='collectionPanel__collection' style={{flexWrap:'wrap', overflow:'hidden'}}>
 	      		{
 	      			products.map(prod=>{
 	      				return(
-	      					<div className='newCollection__all--each' key={`${prod.id}`}>
-		      					<h1>{prod.name}</h1>
-		      					{prod.colors.map(color=><p key={`color-${color}`}>{color}</p>)}
-
-		      				</div>
+	      					<div className='collectionPanel__item' key={`newcoll-${prod.name}`} onClick={()=>window.location.href=`/new/${prod.id}`}>
+								<div className='collectionPanel__item--name'>
+									<p>{prod.name}</p>
+								</div>
+								<div className='collectionPanel__item--image'>
+									<img src={prod.imageUrl} alt={prod.name} />
+								</div>
+								
+								<div className='collectionPanel__item--description'>
+									<p>{prod.description}</p>
+								</div>
+								<div className='collectionPanel__item--price'>
+									<p> &euro; {prod.price}</p>
+								</div>
+								
+								<div className='collectionPanel__item--btn'>
+									<div onClick={()=>window.location.href=`/new/:${prod.id}`}>
+										<p>Shop now</p>
+									</div>
+								</div>
+							</div>
+	      					
 		      			)
 	      			})
 	      		}
